367a7a63ac3f4243cd808271e7b089caa8de67e2
carlosvinicius-ai <cvinicius32@hotmail.com>
2024-06-05 20:56:24 -0300

    update Simple Sum

---
+++ beecrowd/Java/simple-sum/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * beecrowd 1003
+ * https://judge.beecrowd.com/en/problems/view/1003
+ *
+ * @ carlosvinicius-ai*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(String.format("SOMA = %d", (A + B)));
+
+    }
+
+}
