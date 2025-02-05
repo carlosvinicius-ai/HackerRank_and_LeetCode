f62916018bb3ce179d520a35448155fa1d923176
carlosvinicius-ai <cvinicius32@hotmail.com>
2025-02-05 20:49:41 -0300

    1014 beecrowd in python

---
+++ beecrowd/Python/1014Consumo.py
@@ -0,0 +1,10 @@
+# beecrowd 1014
+# https://judge.beecrowd.com/pt/problems/view/1014
+# @carlosvinicius-ai
+
+distancia = int(input())
+combustivel = float(input())
+
+consumo = distancia/combustivel
+
+print(f'{consumo:.3f} km/l')
