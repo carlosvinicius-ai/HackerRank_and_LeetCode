49114ea6fb59f259784ac30fcb84741104ac9251
carlosvinicius-ai <cvinicius32@hotmail.com>
2025-02-06 20:21:55 -0300

    1021 beecrowd in python

---
+++ beecrowd/Python/1021CedulasEMoedas.py
@@ -0,0 +1,19 @@
+# beecrowd 1021
+# https://judge.beecrowd.com/pt/problems/view/1021
+# @carlosvinicius-ai
+
+reais, centavos = [int(x) for x in input().strip().split('.')]
+reais = reais * 100 + centavos
+
+notas = [10000, 5000, 2000, 1000, 500, 200]
+moedas = [100, 50, 25, 10, 5, 1]
+
+print("NOTAS:")
+for nota in notas:
+    print(f"{reais//nota} nota(s) de R$ {(nota/100):.2f}")
+    reais %= nota
+
+print("MOEDAS:")
+for moeda in moedas:
+    print(f"{reais//moeda} moeda(s) de R$ {(moeda/100):.2f}")
+    reais %= moeda
