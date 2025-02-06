e05241ce7a4b3cdd0693428be4b0164f48809a08
carlosvinicius-ai <cvinicius32@hotmail.com>
2025-02-06 20:22:33 -0300

    1930 beecrowd in python

---
+++ beecrowd/Python/1930Tomadas.py
@@ -0,0 +1,14 @@
+# beecrowd 1930
+# https://judge.beecrowd.com/pt/problems/view/1930
+# @carlosvinicius-ai
+
+tomadas = input().split(' ')
+
+t1 = int(tomadas[0])
+t2 = int(tomadas[1])
+t3 = int(tomadas[2])
+t4 = int(tomadas[3])
+
+itens = (t1 + t2 + t3 + t4) - 3
+
+print(itens)
